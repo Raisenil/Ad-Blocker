bc487049f0f229095a2f40cf1e10d81a429f145b
Raisenil <raisulislamniloy47@gmail.com>
2022-04-21 18:32:08 +0600

    more list added

--- background.js
+++ background.js
@@ -1,24 +1,14 @@
-const defaultFilters = [
-	"*://*.doubleclick.net/*",
-	"*://partner.googleadservices.com/*",
-	"*://*.googlesyndication.com/*",
-	"*://*.google-analytics.com/*",
-	"*://creative.ak.fbcdn.net/*",
-	"*://*.adbrite.com/*",
-	"*://*.exponential.com/*",
-	"*://*.quantserve.com/*",
-	"*://*.scorecardresearch.com/*",
-	"*://*.zedo.com/*",
-]
-
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) 
 	{ 
+		//console.log("Blocking:", details.url)
 		return {
 			cancel: true 
 			}
 	},
-    { urls: defaultFilters },
+    { 
+		urls: defaultFilters 
+	},
     ["blocking"]
 )
 
